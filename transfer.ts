711ab4927d024a49b39ed4eedb787c71a009dc7c
Vexsky <154293262+vexsky@users.noreply.github.com>
2024-06-23 11:26:49 +0900

    create-token

--- transfer.ts
+++ transfer.ts
@@ -6,6 +6,8 @@ import {
   PublicKey,
 } from "@solana/web3.js";
 import dotenv from "dotenv";
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 dotenv.config();
 
 const suppliedToPubkey = process.argv[2] || null;
@@ -15,30 +17,14 @@ if (!suppliedToPubkey) {
   process.exit(1);
 }
 
-const senderKeypair = process.env.SECRET_KEY;
+const senderKeypair = getKeypairFromEnvironment("SECRET_KEY");
 
 console.log(`suppliedToPubkey: ${suppliedToPubkey}`);
 
 const toPubkey = new PublicKey(suppliedToPubkey);
+
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
 console.log(
-  `Loaded our own keypair, the destination public key, and connected to Solana`
+  `✅ Loaded our own keypair, the destination public key, and connected to Solana`
 );
-
-const transaction = new Transaction();
-const LAMPORTS_TO_SEND = 5000;
-const sendSolInstruction = SystemProgram.transfer({
-  fromPubkey: senderKeypair.publicKey,
-  toPubkey,
-  lamports: LAMPORTS_TO_SEND,
-});
-
-transaction.add(sendSolInstruction);
-
-const signature = await sendAndConfirmTransaction(connection, transaction, [
-  senderKeypair,
-]);
-
-console.log(`Finished! Send ${LAMPORTS_TO_SEND} to the address ${toPubkey}.`);
-console.log(`Transaction signature is ${signature}!`);
